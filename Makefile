6a3f8b5dc04c83810179666e0c7c49352db9fd96
Vadim Godunko <vgodunko@gmail.com>
2023-03-13 22:29:22 +0400

    Minimal build infrastructure.

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+all:
+	gprbuild -P gnat/cgk.gpr
---
+++ source/cgk.ads
@@ -0,0 +1,11 @@
+--
+--  Copyright (C) 2023, Vadim Godunko <vgodunko@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+package CGK is
+
+   pragma Pure;
+
+end CGK;
