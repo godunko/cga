cd488a1fcce360ea11182fb9ab308436012cf905
Vadim Godunko <vgodunko@gmail.com>
2023-04-14 20:06:59 +0400

    Stub for topology module.

---
+++ source/topology/cgk-topology.ads
@@ -0,0 +1,11 @@
+--
+--  Copyright (C) 2023, Vadim Godunko <vgodunko@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+package CGK.Topology is
+
+   pragma Pure;
+
+end CGK.Topology;
